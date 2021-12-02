a0029ea501d09a3acf817c0dc24245d5a051c504
Lucas Souza <lucasp.sdev@gmail.com>
2021-12-01 21:10:33 -0300

    Initial commit

---
+++ src/IPessoa.java
@@ -0,0 +1,6 @@
+public interface IPessoa {
+  public void addPeople(Pessoa p);
+  public void removePeople(int id);
+  public void updatePeople(int i, Pessoa p);
+  public void showPeople();
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+  private String name;
+  private String adress;
+  private Integer phone;
+
+  public Integer getPhone() {
+    return phone;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAdress() {
+    return adress;
+  }
+
+  public void setAdress(String adress) {
+    this.adress = adress;
+  }
+
+  public void setPhone(Integer phone) {
+    this.phone = phone;
+  }
+  
+}
